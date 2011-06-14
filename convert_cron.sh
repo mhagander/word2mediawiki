378e0f35b8ed0b1881bcb2c9c7d228acfb7a84f9
Magnus Hagander <magnus@hagander.net>
2011-06-14 16:10:11 +0200

    Add the cronjob script

---
+++ convert_cron.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+LOCK=/var/lock/docconvert
+LOG=/usr/local/word2mediawiki/log/word2mediawiki.log
+
+if [ "$(id -nu)" != "docconvert" ]; then
+   echo This script must be run as user docconvert
+   exit 1
+fi
+
+lockfile-create $LOCK
+if [ $? -ne 0 ]; then
+   echo Converter script already running, aborting
+   exit 1
+fi
+lockfile-touch $LOCK &
+LOCKTOUCHPID="$!"
+
+date +"%Y-%m-%d %H:%M:%S Scanning directory..." >> $LOG
+# If there are any files to convert, send them through the converter
+cd /usr/local/word2mediawiki
+FIRST=1
+for F in $(find share -type f) ; do
+   date +"%Y-%m-%d %H:%M:%S Converting $F..." >> $LOG
+   if [ "$FIRST" == "1" ]; then
+      # If there is an instance of openoffice running, get rid of it
+      # (there should never be one)
+      pkill -9 soffice.bin
+
+      # Start open office in listening mode
+      soffice -accept="socket,port=8100;urp;" -norestore -headless >>$LOG
+
+      # For each run, sleep a while to make sure samba has finished
+      # writing the file, and that openoffice has actually started.
+      sleep 5
+      FIRST=0
+   fi
+   python word2mediawiki.py $F >> $LOG 2>&1
+   rm -f $F
+   date +"%Y-%m-%d %H:%M:%S Done with $F, removing." >> $LOG
+done
+
+# Get rid of our instance of openoffice
+pkill soffice.bin
+
+kill $LOCKTOUCHPID
+lockfile-remove $LOCK
