86a3b41135e0e1e9eca32831d2ef6a665faf563d
Knut Ingvald Dietzel <knut.ingvald.dietzel@redpill-linpro.com>
2012-02-09 21:32:38 +0100

    Improve handling of paths with spaces.

--- convert_cron.sh
+++ convert_cron.sh
@@ -3,6 +3,11 @@
 LOCK=/var/lock/docconvert
 LOG=/usr/local/word2mediawiki/log/word2mediawiki.log
 
+# Set the internal field separator (IFS) to not include spaces, enabling
+# use of paths with spaces
+ORIGINAL_IFS=$IFS
+IFS=$'\n'
+
 if [ "$(id -nu)" != "docconvert" ]; then
    echo This script must be run as user docconvert
    exit 1
@@ -35,10 +40,13 @@ for F in $(find share -type f) ; do
       sleep 5
       FIRST=0
    fi
-   python word2mediawiki.py $F >> $LOG 2>&1
+   python word2mediawiki.py "$F" >> $LOG 2>&1
    rm -f $F
    date +"%Y-%m-%d %H:%M:%S Done with $F, removing." >> $LOG
 done
+
+# Restore internal field separator original value
+IFS=$ORIGINAL_IFS
 
 # Get rid of our instance of openoffice
 pkill soffice.bin
